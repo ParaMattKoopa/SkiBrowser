9c5c906f900adedec4b9facf3ee66c05f31e757b
matthew <matt@gbeef.net>
2024-11-14 10:09:19 -0500

    Update background.js

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
       'width': 1100,
       'height': 800
     },
-  "frame": {color: "#909090"},
+  "frame": {color: "#4d668a"},
   "resizable": true,
   });
 });
